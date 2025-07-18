67be3b8c3286e13da6c0d75e7b611c2ec7c2da74
Atif Hassan <atifhassan547@gmail.com>
2025-07-18 18:24:19 +0500

    I made 2nd program

--- Day 1.py
+++ Day 1.py
@@ -1,4 +1,13 @@
+def square_number():
+    try:
+        a = int(input("Enter a number: "))
+        print(a ** 2)
+    except ValueError:
+        print("Please enter a valid integer.")
 
+if __name__ == "__main__":
+    square_number()
+    print("Hello World")
 
 
 
