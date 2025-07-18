7eacffb0291e9565b9b29ee95d008b48b79a1d7d
Atif Hassan <atifhassan547@gmail.com>
2025-07-18 18:40:39 +0500

    #

--- Day 1.py
+++ Day 1.py
@@ -1,11 +1,13 @@
 def square_number():
-    try:
-        a = int(input("Enter a number: "))
-        print(a ** 2)
-    except ValueError:
-        print("Please enter a valid integer.")
+    while True:
+        try:
+            a = int(input("Enter a number: "))
+            return a ** 2
+        except:
+            print("Please enter a valid integer.")
 
 if __name__ == "__main__":
+    
     square_number()
     print("Hello World")
 
