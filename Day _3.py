f7e010c187264e7e9cf5385e19e5b91ce36f1456
Atif Hassan <atifhassan547@gmail.com>
2025-07-19 15:58:16 +0500

    modules program

---
+++ Day _3.py
@@ -0,0 +1,12 @@
+# Pip
+import pandas as pd
+data={
+    "name": ['Atif','Ali','Ahmad'],
+     "age": ['17','18','19']
+}
+df=pd.DataFrame(data)
+print(df)
+df['name']
+df['age']
+df.head(1)
+len(df)
--- Day_2.py
+++ Day_2.py
@@ -1,1 +1,32 @@
+# Modules
+import math
+print(math.sqrt(16))
+print(math.pi)
+print(math.pow(2,3))
+print(math.factorial(5))
+print(math.ceil(1.2))
+print(math.floor(1.2))
+print(math.sin(math.pi/2))
+print(math.cos(math.pi/2))
+print(math.tan(math.pi/2))
+print(math.log(10,2))
+# ismein hum sqrt ko define kr rahe houn aur usko use kr rahe houn
+import random
+print(random.randint(1,4))
+print(random.random())
+import datetime
+now=datetime.datetime.now()
+print("the time is ",now)
+today=datetime.date.today()
+print("the date is ",today)
+import os
+print(os.getcwd())
+import sys
+print("python version",sys.version)
+
+
+
+
+
+
 
