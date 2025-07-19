31b136bc2af7f23ee8a5d1ebc13aedaf8400d9f6
Atif Hassan <atifhassan547@gmail.com>
2025-07-19 14:34:46 +0500

    this is a comment practice

---
+++ Day_1.py
@@ -0,0 +1,11 @@
+#print kar raha houn apna mame
+name="Atif"
+print(name)
+#user se input lena
+num=input("enter number: ")
+print(num)
+#is hash ko single line comment kehte hein.& ''' is ko hum multi line comments kehte hein
+# ,''' comments ko python ignore krta hai aur apne code mein nahi chalata.
+
+
+
